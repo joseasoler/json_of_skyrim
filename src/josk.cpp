247ec6681c51290388f620f708b62b8ee147c01a
José Ángel Soler Ortiz <jose.angel.soler.ortiz+github@pm.me>
2025-05-23 06:03:50 +0200

    Code cleanup.

--- src/josk.cpp
+++ src/josk.cpp
@@ -4,23 +4,6 @@
 
 int main(const int argc, char* argv[])
 {
-	/*
-	if (argc == 2)
-	{
-		// ToDo hardcoded for now.
-		using josk::tes::record_type;
-		// NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-		const auto result = josk::tes::read_file(argv[1], std::unordered_set{record_type::avif});
-		if (result.has_value())
-		{
-			std::cout << static_cast<int>(result->begin()->first) << ", " << result->begin()->second.size() << '\n';
-		}
-		else
-		{
-			std::cerr << result.error() << '\n';
-		}
-	}
-	*/
 	CLI::App app{};
 	josk::cli::args arguments{};
 	josk::cli::configure_cli(app, arguments);
